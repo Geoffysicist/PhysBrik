d9af5f02d568126b678aab996b93a7279d93e4bf
Geoff Goldrick <14170100+ProfStick@users.noreply.github.com>
2020-10-16 21:35:22 +1100

    added uuids

---
+++ physbryk/physbryk_uuids.py
@@ -0,0 +1,13 @@
+physbryk_uuid_dict = {
+    'MotionService': 100,
+    'acceleration':  101,
+    'gyro':          102,
+    'MagnetService': 200,
+    'magnet':        201,
+    'EMRService':    300,
+    'intensity':     301,
+    'spectrum':      302,
+    'proximity':     303,
+    'DummyService':  1000,
+    'dummy':         1001
+}
